9092394e7dcf61b38332eb37ef6d5172e10a032b
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-10-18 13:38:07 -0400

    Final Version 1.0. New: Revision of code, documenting/commenting, addition of removal methods.

--- src/mysavingstester/MySavingsTester.java
+++ src/mysavingstester/MySavingsTester.java
@@ -3,46 +3,126 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mysavingstester;
+
 import java.util.*;
+
 /**
- *
- * @author chhit5249
+ * This program tests the PiggyBank Class.
+ * @author Chris Hitchcock
  */
 public class MySavingsTester {
 
     /**
+     * Testing happens here.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("How much money is currently in your piggybank?");
-        Scanner kb = new Scanner(System.in);
+        //Variable declaration
         double startBal;
-        startBal = kb.nextDouble();
-        PiggyBank mySavings = new PiggyBank(startBal);
         int choice;
         int amount;
         boolean menu = true;
         
-        while (menu == true)
-        {
-            System.out.println("1.\tView Balance\n2.\tAdd pennies\n3.\tAdd nickels\n4.\tAdd dimes"
-                + "\n5.\tAdd quarters\n6.\tAdd loonies\n7.\tAdd toonies\nPress 0 to quit\nEnter your choice: ");
-            choice = kb.nextInt();
-            switch(choice)
-            {
-                case 0: menu = false;break;
-                case 1: System.out.println(mySavings.viewBal());break;
-                case 2: System.out.println("How many pennies will you add?");amount = kb.nextInt();mySavings.addPenny(amount);break;
-                case 3: System.out.println("How many nickels will you add?");amount = kb.nextInt();mySavings.addNickel(amount);break;
-                case 4: System.out.println("How many dimes will you add?");amount = kb.nextInt();mySavings.addDime(amount);break;
-                case 5: System.out.println("How many quarters will you add?");amount = kb.nextInt();mySavings.addQuarter(amount);break;
-                case 6: System.out.println("How many loonies will you add?");amount = kb.nextInt();mySavings.addLoonie(amount);break;
-                case 7: System.out.println("How many toonies will you add?");amount = kb.nextInt();mySavings.addToonie(amount);break;
+        //Prompt for initial balance, and create the PiggyBank.
+        System.out.println("How much money is currently in your piggybank?");
+        Scanner kb = new Scanner(System.in);
+        startBal = kb.nextDouble();
+        PiggyBank mySavings = new PiggyBank(startBal);
+        
+        //Error handling if a non-integer choice is selected
+        try {
+            //While loop that keeps the menu going until user enters 0.
+            while (menu == true) {
+                //Menu, selections 1-7 and then 0. Get choice.
+                System.out.println("1.\tAdd pennies\t2.\tAdd nickels\n3.\tAdd dimes"
+                        + "\t4.\tAdd quarters\n5.\tAdd loonies\t6.\tAdd toonies\n"
+                        + "7.\tRemove pennies\t8.\tRemove nickels\n9.\tRemove dimes\t"
+                        + "10.\tRemove quarters\n11.\tRemove loonies\t12.\tRemove toonies\n"
+                        + "13.\tView Balance\t0.\tQuit\nEnter your choice: ");
+                choice = kb.nextInt();
+
+                //Switch for choice; prompts for amount if adding coins and then executes the respective methods.
+                switch (choice) {
+                    // <editor-fold defaultstate="collapsed" desc="Cases for Switch">
+                    default:
+                        System.out.println("Please enter a valid selection.");
+                        break;
+                    case 0:
+                        System.out.println(mySavings.viewBal(true));
+                        menu = false;
+                        break;
+                    case 1:
+                        System.out.println("How many pennies will you add?");
+                        amount = kb.nextInt();
+                        mySavings.addPenny(amount);
+                        break;
+                    case 2:
+                        System.out.println("How many nickels will you add?");
+                        amount = kb.nextInt();
+                        mySavings.addNickel(amount);
+                        break;
+                    case 3:
+                        System.out.println("How many dimes will you add?");
+                        amount = kb.nextInt();
+                        mySavings.addDime(amount);
+                        break;
+                    case 4:
+                        System.out.println("How many quarters will you add?");
+                        amount = kb.nextInt();
+                        mySavings.addQuarter(amount);
+                        break;
+                    case 5:
+                        System.out.println("How many loonies will you add?");
+                        amount = kb.nextInt();
+                        mySavings.addLoonie(amount);
+                        break;
+                    case 6:
+                        System.out.println("How many toonies will you add?");
+                        amount = kb.nextInt();
+                        mySavings.addToonie(amount);
+                        break;
+                    case 7:
+                        System.out.println("How many pennies will you remove?");
+                        amount = kb.nextInt();
+                        mySavings.remPenny(amount);
+                        break;
+                    case 8:
+                        System.out.println("How many nickels will you remove?");
+                        amount = kb.nextInt();
+                        mySavings.remNickel(amount);
+                        break;
+                    case 9:
+                        System.out.println("How many dimes will you remove?");
+                        amount = kb.nextInt();
+                        mySavings.remDime(amount);
+                        break;
+                    case 10:
+                        System.out.println("How many quarters will you remove?");
+                        amount = kb.nextInt();
+                        mySavings.remQuarter(amount);
+                        break;
+                    case 11:
+                        System.out.println("How many loonies will you remove?");
+                        amount = kb.nextInt();
+                        mySavings.remLoonie(amount);
+                        break;
+                    case 12:
+                        System.out.println("How many toonies will you remove?");
+                        amount = kb.nextInt();
+                        mySavings.remToonie(amount);
+                        break;
+                    case 13:
+                        System.out.println(mySavings.viewBal());
+                        break;
+                        //</editor-fold>
+                }
+
             }
+        } catch (InputMismatchException e) {
+            //Display cause of error and error message.
+            System.out.println("Valid selection not entered.");
+            System.err.println("InputMismatchException: " + e.getMessage());
         }
     }
-    
 }
--- src/mysavingstester/PiggyBank.java
+++ src/mysavingstester/PiggyBank.java
@@ -3,61 +3,197 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mysavingstester;
 
 /**
- *
- * @author chhit5249
+ * This class allows the user to create a PiggyBank; they can add/remove coins to it
+ * and view the balance with the respective methods; initial balance is inputted
+ * <p>
+ * pre: N/A <br>
+ * post: PiggyBank created/modified
+ * @author Chris Hitchcock
  */
 public class PiggyBank {
+
+    //Variable declaration
     private double money;
-    
-    public PiggyBank(double inMon)
-    {
+
+    /**
+     * Constructor to make the PiggyBank <p>
+     * pre: initial balance inputted <br>
+     * post: PiggyBank created
+     * @param inMon Initial Balance
+     */
+    public PiggyBank(double inMon) {
         money = inMon;
     }
-   
-    public double addPenny(int amount)
-    {
-        money += amount*0.01;
+
+    /**
+     * This method allows the user to add pennies to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents added (1cent/penny).
+     * @param amount Amount of coins being added
+     * @return money; current balance
+     */
+    public double addPenny(int amount) {
+        money += amount * 0.01;
         return money;
     }
-    
-    public double addNickel(int amount)
-    {
-        money += amount*0.05;
+
+    /**
+     * This method allows the user to add nickels to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents added (5cents/penny).
+     * @param amount Amount of coins being added
+     * @return money; current balance
+     */
+    public double addNickel(int amount) {
+        money += amount * 0.05;
         return money;
     }
-    
-    public double addDime(int amount)
-    {
-        money += amount*0.1;
+
+    /**
+     * This method allows the user to add dimes to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents added (10cents/penny).
+     * @param amount Amount of coins being added
+     * @return money; current balance
+     */
+    public double addDime(int amount) {
+        money += amount * 0.1;
         return money;
     }
-    
-    public double addQuarter(int amount)
-    {
-        money += amount*0.25;
+
+    /**
+     * This method allows the user to add quarters to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents added (25cents/penny).
+     * @param amount Amount of coins being added
+     * @return money; current balance
+     */
+    public double addQuarter(int amount) {
+        money += amount * 0.25;
         return money;
     }
-    
-    public double addLoonie(int amount)
-    {
+
+    /**
+     * This method allows the user to add loonies to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: dollars added (1dollar/penny).
+     * @param amount Amount of coins being added
+     * @return money; current balance
+     */
+    public double addLoonie(int amount) {
         money += amount;
         return money;
     }
-    
-    public double addToonie(int amount)
-    {
-        money += amount*2;
+
+    /**
+     * This method allows the user to add toonies to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: dollars added (2dollars/penny).
+     * @param amount Amount of coins being added
+     * @return money; current balance
+     */
+    public double addToonie(int amount) {
+        money += amount * 2;
         return money;
     }
     
-    public String viewBal()
-    {
-        String output = "You have $"+money+" in your piggy bank.";
+    /**
+     * This method allows the user to remove pennies to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents removed (1cent/penny).
+     * @param amount Amount of coins being removed
+     * @return money; current balance
+     */
+    public double remPenny(int amount) {
+        money -= amount * 0.01;
+        return money;
+    }
+
+    /**
+     * This method allows the user to remove nickels to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents removed (5cents/penny).
+     * @param amount Amount of coins being removed
+     * @return money; current balance
+     */
+    public double remNickel(int amount) {
+        money -= amount * 0.05;
+        return money;
+    }
+
+    /**
+     * This method allows the user to remove dimes to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents removed (10cents/penny).
+     * @param amount Amount of coins being removed
+     * @return money; current balance
+     */
+    public double remDime(int amount) {
+        money -= amount * 0.1;
+        return money;
+    }
+
+    /**
+     * This method allows the user to remove quarters to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: cents removed (25cents/penny).
+     * @param amount Amount of coins being removed
+     * @return money; current balance
+     */
+    public double remQuarter(int amount) {
+        money -= amount * 0.25;
+        return money;
+    }
+
+    /**
+     * This method allows the user to remove loonies to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: dollars removed (1dollar/penny).
+     * @param amount Amount of coins being removed
+     * @return money; current balance
+     */
+    public double remLoonie(int amount) {
+        money -= amount;
+        return money;
+    }
+
+    /**
+     * This method allows the user to remove toonies to the Piggy Bank <p>
+     * pre: amount inputted <br>
+     * post: dollars removed (2dollars/penny).
+     * @param amount Amount of coins being removed
+     * @return money; current balance
+     */
+    public double remToonie(int amount) {
+        money -= amount * 2;
+        return money;
+    }
+
+    /**
+     * This method sets the balance to a String so it can be outputted <p>
+     * pre: N/A <br>
+     * post: output returned - balance as a string
+     * @return output; money as a String
+     */
+    public String viewBal() {
+        String output = "You have $" + money + " in your piggy bank.";
         return output;
     }
-    
+
+    /**
+     * This method sets the balance to a String so it can be outputted, however
+     * the statement for this one is final <p>
+     * pre: boolean inputted <br>
+     * post: output returned - balance as a string
+     * @return output; money as a String
+     * @param fin Boolean only inputted if program is done.
+     */
+    public String viewBal(boolean fin) {
+        String output = "Your final balance in the Piggy Bank is :$" + money;
+        return output;
+    }
+
 }
